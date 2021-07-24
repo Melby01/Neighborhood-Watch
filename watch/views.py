d4cd1a5003eeae28bff0a100a0889f92076f4dcc
Melby01 <melby.okozi@student.moringaschool.com>
2021-07-24 22:59:21 +0300

    Registration form update

--- watch/views.py
+++ watch/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from django.contrib.auth import login, authenticate
+from .forms import RegistrationForm
+from django.contrib import messages
 
 # Create your views here.
+def index(request):
+    return render(request, 'index.html')
+
+def register(request):
+    if request.method == 'POST':
+     form = RegistrationForm(request.POST)
+     if form.is_valid():
+      form.save()
+      username = form.cleaned_data.get('username')
+      password = form.cleaned_data.get('password')
+      user = authenticate(username=username, password=password)
+      messages.success(request,f'Account for {username} created,you can now login')
+      login(request, user)
+      return redirect('index.html')
+    else:
+      form = RegistrationForm()
+    return render(request,'registration/register.html',{"form":form})
